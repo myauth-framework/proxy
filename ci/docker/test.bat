927986455b019bf0ce7f03e2a21869c07555e343
Andrey O <ozzy.ext@gmail.com>
2021-01-13 15:48:51 +0300

    fix docker publish

--- ci/docker/test.bat
+++ ci/docker/test.bat
@@ -2,9 +2,9 @@
 
 IF [%1]==[] goto noparam
 
-for %%I in (..) do set ParentFolderName=%%~fI
+xcopy /E /I /Y ..\..\test .\out\test
 
-docker run -v %ParentFolderName%\test:/app/libs/test --rm ozzyext/myauth-proxy:%1 bash -c "cd /app/libs/test; bash test.sh"
+docker run -v %cd%\out\test:/app/libs/test --rm ozzyext/myauth-proxy:%1 bash -c "cd /app/libs/test; bash test.sh"
 
 goto done
 
