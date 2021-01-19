1e9c18bb31a01984b187a0455b6ea55588a78a7a
Andrey O <ozzy.ext@gmail.com>
2021-01-19 23:29:08 +0300

    Secrets refatoring

--- src/myauth/secrets.lua
+++ src/myauth/secrets.lua
@@ -3,16 +3,20 @@
 local _M = {}
 
 function _M.load(filepath)
+	
+	local secrets = {}
 	local configEnv = {}
 
 	local f,err = loadfile(filepath, "t", configEnv)
 
 	if f then
 	   f()
-	   _M.jwt_secret = configEnv.jwt_secret
+	   secrets.jwt_secret = configEnv.jwt_secret
 	else
 	   error(err)
 	end
+
+	return secrets
 end
 
 return _M
