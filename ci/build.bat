0df21bbb7238875ee8a25a94765aa8d78b64d26b
Andrey O <ozzy.ext@gmail.com>
2021-01-20 20:24:16 +0300

    Test refactoring and add metrics_event_listener

--- ci/build.bat
+++ ci/build.bat
@@ -4,6 +4,6 @@ echo "Copy sources ..."
 xcopy /E /I /Y ..\src .\out
 
 echo "Build image ..."
-docker build --no-cache --build-arg VERSION=1.0.0 -t ozzyext/myauth-proxy:1.8.6 -t ozzyext/myauth-proxy:latest .
+docker build --no-cache --build-arg VERSION=1.0.1 -t ozzyext/myauth-proxy:1.8.6 -t ozzyext/myauth-proxy:latest .
 
 echo "Done!"
